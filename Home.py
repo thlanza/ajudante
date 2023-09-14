7f0d0504f0dcfb39e573a0c259c80d98accb7cd1
Thiago Lanza <thlanza@hotmail.com>
2023-09-14 18:29:27 -0300

    multipage native

--- Home.py
+++ Home.py
@@ -1,16 +1,10 @@
 import streamlit as st
-from st_pages import Page, add_page_title, show_pages
 
-add_page_title()  
-
-show_pages(
-    [
-        Page("app.py", "Home", "🏠"),
-        Page("pages/resumo.py", "Resumo", ":books:"),
-        Page("pages/traducao.py", "Traducao", ":page_facing_up:"),
-    ]
+st.set_page_config(
+    page_title="Ajudante"
 )
 
+st.sidebar.success("Selecione uma página abaixo")
 
 st.image("https://www.viajenaviagem.com/wp-content/uploads/2020/02/belo-horizonte-pampulha.jpg.webp", caption='Autoria de Thiago Lanza. Todos os direitos reservados')
 st.header("Esse é o ajudante!")
